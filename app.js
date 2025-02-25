85415e364bef3baf8972af2bda35d362fe91914c
Rafael Rodrigues <rafaa.cfc@gmail.com>
2025-02-25 05:32:54 -0300

    config cors

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ const app = express();
 
 app.use(express.json());
 
+
+app.use((req, res, next) => {  
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir todos os domínios  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos permitidos  
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos  
+  next();  
+});  
+
 // Rotas
 app.use('/api/waitingListAccount', routes);
 // Configuração do Swagger
